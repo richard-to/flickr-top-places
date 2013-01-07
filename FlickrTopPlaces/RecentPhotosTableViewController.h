134524e05d6c3aca018f4d6325d09f0e32a84bfe
richard-to <gohn76@gmail.com>
2013-01-06 22:59:55 -0900

    Add boilerplate RecentPhotosTableViewController

---
+++ FlickrTopPlaces/RecentPhotosTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecentPhotosTableViewController.h
+//  FlickrTopPlaces
+//
+//  Created by Richard To on 1/6/13.
+//  Copyright (c) 2013 Richard To. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecentPhotosTableViewController : UITableViewController
+
+@end
