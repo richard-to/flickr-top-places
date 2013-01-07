f457b4926919028fa0ee6668c2a03ab225f7dcb5
richard-to <gohn76@gmail.com>
2013-01-07 00:51:31 -0900

    Add PhotoViewer Controller and Storyboard setup

---
+++ FlickrTopPlaces/PhotoViewerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoViewerViewController.h
+//  FlickrTopPlaces
+//
+//  Created by Richard To on 1/7/13.
+//  Copyright (c) 2013 Richard To. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoViewerViewController : UIViewController
+
+@end
---
+++ FlickrTopPlaces/PhotoViewerViewController.m
@@ -0,0 +1,38 @@
+//
+//  PhotoViewerViewController.m
+//  FlickrTopPlaces
+//
+//  Created by Richard To on 1/7/13.
+//  Copyright (c) 2013 Richard To. All rights reserved.
+//
+
+#import "PhotoViewerViewController.h"
+
+@interface PhotoViewerViewController ()
+
+@end
+
+@implementation PhotoViewerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
