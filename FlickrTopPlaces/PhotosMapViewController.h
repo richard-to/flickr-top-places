64181e0164c39df6711fceba10d332e36229a13a
richard-to <gohn76@gmail.com>
2013-01-12 11:53:33 -0900

    Add boiler plate photo map view.
    Should probably subclass this.

---
+++ FlickrTopPlaces/PhotosMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotosMapViewController.h
+//  FlickrTopPlaces
+//
+//  Created by Richard To on 1/12/13.
+//  Copyright (c) 2013 Richard To. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotosMapViewController : UIViewController
+
+@end
